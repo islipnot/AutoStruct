35e767ad9c2aea7d25d9dd8537ffac0f8816295c
vyxblvs <newdiscord69420@proton.me>
2024-10-07 22:40:01 -0700

    cpp struct/enum alignment added, flags and typedef conversion not yet finished

--- AutoStruct/main.cpp
+++ AutoStruct/main.cpp
@@ -1,10 +1,18 @@
 #include "pch.h"
 
+enum ARG_FLAGS
+{
+	HexVariables     = 0,
+	RemoveWhitespace = 1,
+	ConvertTypedef   = 2
+};
+
 void AlignAndPrint(std::vector<std::string>& lines, const size_t LongestName)
 {
 	for (std::string& line : lines)
 	{
-		const size_t EqualPos = line.find_first_of('=') - 1;
+		const size_t EqualPos = line.find_first_of('=');
+
 		if (EqualPos < LongestName)
 		{
 			line.insert(EqualPos, LongestName - EqualPos, ' ');
@@ -14,6 +22,41 @@ void AlignAndPrint(std::vector<std::string>& lines, const size_t LongestName)
 	}
 
 	std::cout << "};\n";
+}
+
+void HandleCppData(std::ifstream& file)
+{
+	std::string line;
+
+	std::vector<std::string> lines;
+	size_t LongestName = 0;
+	
+	while (std::getline(file, line) && line.find('}') == std::string::npos)
+	{
+		size_t pos = line.find_first_not_of(' ');
+		if (pos == 0)
+		{
+			pos = line.find_first_not_of('\t');
+		}
+
+		if (pos != std::string::npos)
+		{
+			line.erase(0, pos);
+		}
+
+		line.insert(0, 4, ' ');
+
+		pos = line.find_first_of('=');
+		if (pos != std::string::npos)
+		{
+			pos += 2;
+			if (pos > LongestName) LongestName = pos;
+		}
+
+		lines.emplace_back(line);
+	}
+
+	AlignAndPrint(lines, LongestName - 4);
 }
 
 void CvtIdaEnum(std::ifstream& file, size_t start)
@@ -27,7 +70,7 @@ void CvtIdaEnum(std::ifstream& file, size_t start)
 	while (std::getline(file, line))
 	{
 		line.erase(0, start);
-		line.insert(0, "   ");
+		line.insert(0, "   "); // Visual Studio's indentation
 
 		const size_t CommentPos = line.find_first_of(';');
 		if (CommentPos != std::string::npos)
@@ -37,7 +80,7 @@ void CvtIdaEnum(std::ifstream& file, size_t start)
 		}
 
 		const size_t NumStart = line.find_first_of('=') + 2;
-		if (NumStart > LongestName) LongestName = NumStart;
+		if (NumStart > LongestName) LongestName = NumStart + 1;
 
 		const size_t NumEnd = line.find_first_of(' ', NumStart);
 		if (NumEnd != std::string::npos) line.erase(NumEnd);
@@ -65,15 +108,13 @@ void CvtIdaEnum(std::ifstream& file, size_t start)
 
 int wmain(int argc, wchar_t* argv[])
 {
-	/* TODO
-	* - Add back flags that allow more customizable output, and handle whitespace/comments better
-	*/
-
 	if (argc < 2)
 	{
 		std::cout << "Insufficient arguments\n";
 		return 1;
 	}
+
+	// Getting file and first line
 
 	std::ifstream file(argv[1]);
 	if (file.fail())
@@ -82,8 +123,26 @@ int wmain(int argc, wchar_t* argv[])
 		return 1;
 	}
 
-	std::string line;
+	std::string line, FirstLine;
 	std::getline(file, line);
+
+	// Getting flags
+
+	int flags = 0;
+
+	for (int i = 2; i < argc; ++i)
+	{
+		if (_wcsicmp(argv[i], L"convert") == 0)
+			flags |= ConvertTypedef;
+
+		else if (_wcsicmp(argv[i], L"rws") == 0)
+			flags |= RemoveWhitespace;
+
+		else if (_wcsicmp(argv[i], L"hex") == 0)
+			flags |= HexVariables;
+	}
+
+	// Handling the provided data accordingly
 
 	switch (line[0])
 	{
@@ -102,12 +161,22 @@ int wmain(int argc, wchar_t* argv[])
 
 	case 't': // typedef
 	{
-		break;
+		if (flags & ConvertTypedef) line.erase(0, 8);
+		[[fallthrough]];
 	}
 
 	default: // enum/struct
 	{
+		std::cout << line;
 
+		if (line.find('{') == std::string::npos)
+		{
+			std::getline(file, line);
+			std::cout << "\n{\n";
+		}
+		else std::cout << '\n';
+
+		HandleCppData(file);
 	}
 	}
 
--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Automatic alignment and syntax conversion of IDA/C++ enums and structs.
 
 ## Usage
-Argument format: &lt;FilePath&gt;</br>
-The type of structure will automatically be detected, and alignment/conversion will be applied
+Argument format: &lt;FilePath&gt; &lt;Flags&gt;</br>
+The type of structure will automatically be detected, and alignment/conversion will be applied</br></br>
+
+Arguments (Case-Insensitive)
+- "Convert": formats the struct/enum typedef's for IDA, so that the struct and its typedef's can be inserted at once into local types.
+- "Rws": Removes empty lines (rws = remove whitespace)
+- "Hex": Converts all decimal to hexidecimal
